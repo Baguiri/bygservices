78c79edfbc5b299eb2e096efca0d8749e62af871
Baguiri <baguiri9473@gmail.com>
2020-09-09 03:45:11 +0300

    Updates

---
+++ precache-manifest.ff8bb5cf65e49d7f394fef449ae5affe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9974f32471f3b4ddb8c760ac60f2719",
+    "url": "/bygservices/index.html"
+  },
+  {
+    "revision": "fa5f3b4f61d6f1594388",
+    "url": "/bygservices/static/css/2.7ec6336c.chunk.css"
+  },
+  {
+    "revision": "f5897c6893d96c53011c",
+    "url": "/bygservices/static/css/main.fbb5d3c4.chunk.css"
+  },
+  {
+    "revision": "fa5f3b4f61d6f1594388",
+    "url": "/bygservices/static/js/2.3f4be62e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bygservices/static/js/2.3f4be62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5897c6893d96c53011c",
+    "url": "/bygservices/static/js/main.dedec1e3.chunk.js"
+  },
+  {
+    "revision": "29d94ec56813db4dfede",
+    "url": "/bygservices/static/js/runtime-main.34cd7172.js"
+  },
+  {
+    "revision": "188f43d2df5014ef9a54359d2e9d902b",
+    "url": "/bygservices/static/media/MTS.188f43d2.jpg"
+  },
+  {
+    "revision": "41db73ab32e8c919cb9958ece6fb8596",
+    "url": "/bygservices/static/media/baguiri.41db73ab.jpg"
+  },
+  {
+    "revision": "9119f9968df20801fffd6bed20b4a552",
+    "url": "/bygservices/static/media/container.9119f996.jpg"
+  },
+  {
+    "revision": "b27d23f4cb7d2d0375aee313772e8e49",
+    "url": "/bygservices/static/media/container1.b27d23f4.jpg"
+  },
+  {
+    "revision": "66ba5528d1bf0a2edd312cdacf7614f4",
+    "url": "/bygservices/static/media/eras1.66ba5528.jpg"
+  },
+  {
+    "revision": "45332362ac27a668b7b8aa39e244ca88",
+    "url": "/bygservices/static/media/logo.45332362.jpg"
+  },
+  {
+    "revision": "6d2165d8545c6b6b41ceaef88691d5bb",
+    "url": "/bygservices/static/media/logo1.6d2165d8.jpg"
+  },
+  {
+    "revision": "6a2b88e3c36e8d0e153f915965d43d9d",
+    "url": "/bygservices/static/media/sky.6a2b88e3.jpg"
+  },
+  {
+    "revision": "7d03ccda9478ef8d49eb7453d7b83cca",
+    "url": "/bygservices/static/media/warehouse1.7d03ccda.jpg"
+  }
+]);
